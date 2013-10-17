b07d574d3bc48c35e250a033dc955f7389a35edd
vmp <vmp@live.ru>
2013-10-17 13:30:34 +0300

    Инициализирована библиотека VuzoUI.

---
+++ VuzoUI/MyClass.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 17.10.2013
+ * Time: 13:29
+ */
+using System;
+using System.Collections.Generic;
+
+namespace VuzoUI
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
