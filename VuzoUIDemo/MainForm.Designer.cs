cb60bb2bebc8fda98af79e57b5241c2d2296a41b
vmp <vmp@live.ru>
2013-10-17 14:24:08 +0300

    В пример добавлен другой цвет кнопок и возможность ручной установки скругления.

--- VuzoUIDemo/MainForm.Designer.cs
+++ VuzoUIDemo/MainForm.Designer.cs
@@ -37,12 +37,16 @@ namespace VuzoUIDemo
 			this.myButton1 = new VuzoUI.Controls.MyButton();
 			this.myButton2 = new VuzoUI.Controls.MyButton();
 			this.myButton3 = new VuzoUI.Controls.MyButton();
+			this.myButton4 = new VuzoUI.Controls.MyButton();
+			this.myButton5 = new VuzoUI.Controls.MyButton();
+			this.myButton6 = new VuzoUI.Controls.MyButton();
 			this.SuspendLayout();
 			// 
 			// myButton1
 			// 
 			this.myButton1.Location = new System.Drawing.Point(16, 16);
 			this.myButton1.Name = "myButton1";
+			this.myButton1.Rounded = 7F;
 			this.myButton1.Size = new System.Drawing.Size(75, 23);
 			this.myButton1.TabIndex = 0;
 			this.myButton1.Text = "myButton1";
@@ -52,6 +56,7 @@ namespace VuzoUIDemo
 			// 
 			this.myButton2.Location = new System.Drawing.Point(104, 16);
 			this.myButton2.Name = "myButton2";
+			this.myButton2.Rounded = 7F;
 			this.myButton2.Size = new System.Drawing.Size(75, 23);
 			this.myButton2.TabIndex = 1;
 			this.myButton2.Text = "myButton2";
@@ -61,16 +66,62 @@ namespace VuzoUIDemo
 			// 
 			this.myButton3.Location = new System.Drawing.Point(192, 16);
 			this.myButton3.Name = "myButton3";
+			this.myButton3.Rounded = 7F;
 			this.myButton3.Size = new System.Drawing.Size(75, 23);
 			this.myButton3.TabIndex = 2;
 			this.myButton3.Text = "myButton3";
 			this.myButton3.UseVisualStyleBackColor = true;
+			// 
+			// myButton4
+			// 
+			this.myButton4.BGGradBot = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+			this.myButton4.BGGradTop = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+			this.myButton4.Location = new System.Drawing.Point(192, 48);
+			this.myButton4.Name = "myButton4";
+			this.myButton4.OutLine = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+			this.myButton4.Rounded = 7F;
+			this.myButton4.Size = new System.Drawing.Size(75, 23);
+			this.myButton4.TabIndex = 5;
+			this.myButton4.Text = "myButton4";
+			this.myButton4.TextColor = System.Drawing.Color.Black;
+			this.myButton4.UseVisualStyleBackColor = true;
+			// 
+			// myButton5
+			// 
+			this.myButton5.BGGradBot = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+			this.myButton5.BGGradTop = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+			this.myButton5.Location = new System.Drawing.Point(104, 48);
+			this.myButton5.Name = "myButton5";
+			this.myButton5.OutLine = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+			this.myButton5.Rounded = 7F;
+			this.myButton5.Size = new System.Drawing.Size(75, 23);
+			this.myButton5.TabIndex = 4;
+			this.myButton5.Text = "myButton5";
+			this.myButton5.TextColor = System.Drawing.Color.Black;
+			this.myButton5.UseVisualStyleBackColor = true;
+			// 
+			// myButton6
+			// 
+			this.myButton6.BGGradBot = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+			this.myButton6.BGGradTop = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+			this.myButton6.Location = new System.Drawing.Point(16, 48);
+			this.myButton6.Name = "myButton6";
+			this.myButton6.OutLine = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+			this.myButton6.Rounded = 7F;
+			this.myButton6.Size = new System.Drawing.Size(75, 23);
+			this.myButton6.TabIndex = 3;
+			this.myButton6.Text = "myButton6";
+			this.myButton6.TextColor = System.Drawing.Color.Black;
+			this.myButton6.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(576, 344);
+			this.Controls.Add(this.myButton4);
+			this.Controls.Add(this.myButton5);
+			this.Controls.Add(this.myButton6);
 			this.Controls.Add(this.myButton3);
 			this.Controls.Add(this.myButton2);
 			this.Controls.Add(this.myButton1);
@@ -78,6 +129,9 @@ namespace VuzoUIDemo
 			this.Text = "VuzoUIDemo";
 			this.ResumeLayout(false);
 		}
+		private VuzoUI.Controls.MyButton myButton6;
+		private VuzoUI.Controls.MyButton myButton5;
+		private VuzoUI.Controls.MyButton myButton4;
 		private VuzoUI.Controls.MyButton myButton3;
 		private VuzoUI.Controls.MyButton myButton2;
 		private VuzoUI.Controls.MyButton myButton1;
